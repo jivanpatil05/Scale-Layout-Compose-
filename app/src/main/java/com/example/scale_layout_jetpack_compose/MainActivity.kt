799cc7f5b55871ec7f44efc907dd2cf5e91ff278
Jivan <g1patil0509@gmail.com>
2024-10-22 13:07:11 +0530

    First Commi

--- app/src/main/java/com/example/scale_layout_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/scale_layout_jetpack_compose/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import com.example.scale_layout_jetpack_compose.ui.theme.Scale_Layout_jetpack_composeTheme
